6da8395e7997f6cbe51d248bce0645761ea8a20d
AlexMantoux <alex.mantoux@gmail.com>
2025-11-11 19:37:50 +0100

    add count

--- ft_printf.c
+++ ft_printf.c
@@ -6,25 +6,32 @@
 /*   By: amantoux <amantoux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 11:14:55 by amantoux          #+#    #+#             */
-/*   Updated: 2025/11/11 19:17:25 by amantoux         ###   ########.fr       */
+/*   Updated: 2025/11/11 19:37:28 by amantoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdarg.h>
 
-void	format_c(char c)
+int	format_c(char c)
 {
 	write(1, &c, 1);
+	return (1);
 }
 
-void	format_s(char *str)
+int	format_s(char *str)
 {
+	int	count;
 	int	i;
 
+	count = 0;
 	i = 0;
 	while (str[i])
+	{
 		write(1,&str[i++],1);
+		count++;
+	}
+	return (count);
 }
 
 void	format_d(int n)
@@ -87,7 +94,7 @@ void	format_p(void *p)
 	else
 	{
 		format_s("0x");
-		format_x(ad);	
+		format_x(ad);
 	}
 }
 
@@ -95,20 +102,22 @@ int	ft_printf(const char *c, ...)
 {
 	va_list args;
 	int		i;
+	int		count;
 
 	va_start(args, c);
 	i = 0;
+	count = 0;
 	while (c[i])
 	{
 		if (c[i] == '%' && c[i + 1])
 		{
 			i++;
 			if (c[i] == '%')
-				format_c('%');
+				count += format_c('%');
 			if (c[i] == 'c')
-				format_c(va_arg(args, int));
+				count += format_c(va_arg(args, int));
 			if (c[i] == 's')
-				format_s(va_arg(args, char *));
+				count += format_s(va_arg(args, char *));
 			if (c[i] == 'd' || c[i] == 'i')
 				format_d(va_arg(args, int));
 			if (c[i] == 'u')
@@ -121,26 +130,32 @@ int	ft_printf(const char *c, ...)
 				format_p(va_arg(args, void *));
 		}
 		else
+		{
+			count++;
 			format_c(c[i]);
+		}
 		i++;
 	}
 	va_end(args);
-	return (1);
+	return (count);
 }
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int	main(void)
-// {
-// 	int x = 42;
-// 	char *str = "Alex";
+int	main(void)
+{
+	int x = 42;
+	char *str = "Alex";
 
-// 	ft_printf("Adresse de x : %p\n", &x);
-// 	ft_printf("Adresse de str : %p\n", str);
-// 	ft_printf("Pointeur NULL : %p\n", NULL);
+	// ft_printf("Adresse de x : %p\n", &x);
+	// ft_printf("Adresse de str : %p\n", str);
+	// ft_printf("Pointeur NULL : %p\n", NULL);
 	
-// 	printf("Adresse de x : %p\n", &x);
-// 	printf("Adresse de str : %p\n", str);
-// 	printf("Pointeur NULL : %p\n", NULL);
-// 	return (0);
-// }
+	// printf("Adresse de x : %p\n", &x);
+	// printf("Adresse de str : %p\n", str);
+	// printf("Pointeur NULL : %p\n", NULL);
+
+	printf("%d\n", ft_printf("alex %s", "alex"));
+	printf("%d", printf("alex %s", "alex"));
+	return (0);
+}
